c53c95c5670c368740bff816307388ae37865188
norasdataanalyst <norasdataanalyst@gmail.com>
2024-07-30 17:57:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ if list(df.columns) != expected_columns:
 # Page d'authentification
 def authentification():
     st.title("Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+    username = st.text_input("Noras")
+    password = st.text_input("1234", type="password")
     if st.button("Login"):
         user = df[(df['name'] == username) & (df['password'] == password)]
         if not user.empty:
